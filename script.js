8fe587df4a4bf34b276ea4e59932fae65b588e4b
hyphenminus <36642485+hyphenminus@users.noreply.github.com>
2021-08-30 16:24:39 +0100

    created random weather message

--- script.js
+++ script.js
@@ -2,5 +2,29 @@
 /*
  Mixed Messages coding project 
  https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages 
-*/
-console.log("Testing script.js");
+ Generate a random weather message
+ */
+const dayMessage = () => {
+    const chooseItem = (array) => {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    const precipitations = ["dry", "showery", "rainy", "snowy", "drizzly", "stormy"];
+    const temperatures = ["freezing", "cold", "chilly", "mild", "hot", "scorching"];
+    const windStrengths = ["strong", "blustery", "slight"];
+    const windSources = ["north", "north-east", "east", "south-east", "south", "south-west", "west", "north-west"];
+    const precipitation = chooseItem(precipitations);
+    const temperature = chooseItem(temperatures);
+    const windStrength = chooseItem(windStrengths);
+    const windSource = chooseItem(windSources);
+    const message = `${precipitation} with ${temperature} temperatures and ${windStrength} winds from the ${windSource}`;
+    return message;
+};
+const weatherMessages = () => {
+    const today = dayMessage();
+    const tomorrow = dayMessage();
+    const weekend = dayMessage();
+    const message = `The weather today will be ${today}. Tomorrow's weather will be ${tomorrow}. `
+        + `The outlook for the weekend is ${weekend}. So do wrap up.`;
+    return message;
+};
+console.log(weatherMessages());
