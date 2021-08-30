e4e75263399f9af49e551a744caf2c35e9f600a8
hyphenminus <36642485+hyphenminus@users.noreply.github.com>
2021-08-30 15:25:10 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+/*
+ Mixed Messages coding project 
+ https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages 
+*/
+console.log("Testing script.js");
